92873196349aee891179e738aeda55ab530faec2
emmanuelselicour <emmanuelselicour.2002@gmail.com>
2025-11-02 21:34:26 -0500

    Create currencies.js

---
+++ src/utils/currencies.js
@@ -0,0 +1,8 @@
+export const currencies = [
+  { code: 'EUR', name: 'Euro', symbol: '€' },
+  { code: 'USD', name: 'US Dollar', symbol: '$' },
+  { code: 'CAD', name: 'Canadian Dollar', symbol: 'C$' },
+  { code: 'HTG', name: 'Haitian Gourde', symbol: 'G' },
+  { code: 'DOP', name: 'Dominican Peso', symbol: 'RD$' },
+  { code: 'GBP', name: 'British Pound', symbol: '£' }
+];
